a007e37e478d5321680b6b718c4dcb44195d0f04
namark <namark@disroot.org>
2019-03-17 03:20:54 +0400

    Range intersection unit test.

---
+++ unit_tests/range_intersection.cpp
@@ -0,0 +1,13 @@
+#include <cassert>
+#include "simple/support/range.hpp"
+#include "simple/geom/vector.hpp"
+
+using namespace simple;
+using namespace geom;
+using namespace support;
+
+int main()
+{
+	#include "range_intersection.data"
+	return 0;
+}
